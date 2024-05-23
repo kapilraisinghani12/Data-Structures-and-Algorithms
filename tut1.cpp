9a343ca309b09c8b0f0cc23ddd7b9774fd375e58
kapilraisinghani12 <kapil.219303130@muj.manipal.edu>
2024-05-23 11:47:45 +0530

    c-2

--- tut1.cpp
+++ tut1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+   int findMax(int *arr, int s){
+     
+     if(s == 1){
+        return arr[0];
+     }
+     arr[1] = max(arr[0], arr[1]);
+     findMax(arr+1, s-1);
+     
+
+   }
+
 int main(){
-   int arr[11] = {13,5,4,2,7,55,43,87,23,55,12};
-   
+   int arr[10] = {13,5,4,2,7,92,43,87,23,12};
+   cout<<findMax(arr, 10)<<endl;
 
 
 
